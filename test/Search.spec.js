90d15b0d205dbda60d6e0d1309ee978ef94dcfa8
naren <mknnaren@cloudmpower.com>
2021-02-12 23:43:39 +0530

    test script update

--- test/Search.spec.js
+++
@@ -1,9 +0,0 @@
-import { mount } from '@vue/test-utils'
-import Search from '@/components/movie/search.vue'
-
-describe('Search', () => {
-  test('is a Vue instance', () => {
-    const wrapper = mount(Search)
-    expect(wrapper.vm).toBeTruthy()
-  })
-})
